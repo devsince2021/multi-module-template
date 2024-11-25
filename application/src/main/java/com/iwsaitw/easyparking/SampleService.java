aae5d9d91e678c3ef94d88d8338bb5cd415d3e47
devsince2021 <dev.ryu2021@gmail.com>
2024-11-25 22:12:28 +0900

    feat: 어플리케이션 모듈 추가

---
+++ application/src/main/java/com/iwsaitw/easyparking/SampleService.java
@@ -0,0 +1,11 @@
+package com.iwsaitw.easyparking;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+    public String test() {
+        return "work";
+    }
+}
