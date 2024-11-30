7d06ddd51f73a1ad9b841e667445911c51174d91
devsince2021 <dev.ryu2021@gmail.com>
2024-11-30 15:52:51 +0900

    refactor: core 를 통해서 application에서 exception 사용할 수 있도록 설정

--- application/src/main/java/com/iwsaitw/easyparking/application/SampleService.java
+++ application/src/main/java/com/iwsaitw/easyparking/application/SampleService.java
@@ -1,6 +1,7 @@
 package com.iwsaitw.easyparking.application;
 
 import com.iwsaitw.easyparking.core.domain.SampleDomain;
+import com.iwsaitw.utils.exception.BaseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,10 @@ public class SampleService {
     public String test() {
         SampleDomain instance = new SampleDomain("work");
         return instance.getName();
+    }
+
+    public void throwBaseException() {
+        throw new BaseException("test", "it is test");
     }
 
     public void createSample() {
