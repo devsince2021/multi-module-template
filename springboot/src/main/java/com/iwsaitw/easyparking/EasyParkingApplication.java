14f7e718d811ea8e04f73b7c612026bbe2d3bebb
devsince2021 <dev.ryu2021@gmail.com>
2024-11-30 15:07:21 +0900

    refactor: 패키지 이름 변경

--- springboot/src/main/java/com/iwsaitw/easyparking/EasyParkingApplication.java
+++ springboot/src/main/java/com/iwsaitw/easyparking/EasyParkingApplication.java
@@ -1,11 +1,10 @@
-package com.iwsaitw.easyparking.api;
+package com.iwsaitw.easyparking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.iwsaitw.easyparking")
 public class EasyParkingApplication {
     public static void main(String[] args) {
         SpringApplication.run(EasyParkingApplication.class, args);
