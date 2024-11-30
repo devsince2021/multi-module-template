c2a60c1da78c782daa376476f866d6ed338d22a2
devsince2021 <dev.ryu2021@gmail.com>
2024-11-30 15:05:40 +0900

    add: 테스트 환경 분리

--- springboot/src/test/java/com/iwsaitw/easyparking/EasyParkingApplicationTest.java
+++ springboot/src/test/java/com/iwsaitw/easyparking/EasyParkingApplicationTest.java
@@ -2,7 +2,9 @@ package com.iwsaitw.easyparking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 public class EasyParkingApplicationTest {
 
