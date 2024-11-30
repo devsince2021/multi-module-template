09b0789587d94fd991d58a13cc1e7f0cfb65acee
devsince2021 <dev.ryu2021@gmail.com>
2024-11-30 14:40:21 +0900

    add: 레포지토리 사용을 위한 인터페이스 작성

---
+++ application/src/main/java/com/iwsaitw/easyparking/application/SampleRepository.java
@@ -0,0 +1,8 @@
+package com.iwsaitw.easyparking.application;
+
+
+import com.iwsaitw.easyparking.core.domain.SampleDomain;
+
+public interface SampleRepository {
+    SampleDomain save(SampleDomain domain);
+}
