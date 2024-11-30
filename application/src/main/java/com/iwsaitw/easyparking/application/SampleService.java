282a9791c94a4be2a95fd81c5676a600b082eba8
devsince2021 <dev.ryu2021@gmail.com>
2024-11-30 14:37:07 +0900

    add: 샘플 도메인 객체 생성

--- application/src/main/java/com/iwsaitw/easyparking/application/SampleService.java
+++ application/src/main/java/com/iwsaitw/easyparking/application/SampleService.java
@@ -1,14 +1,13 @@
 package com.iwsaitw.easyparking.application;
 
-import com.iwsaitw.easyparking.core.SampleDomain;
+import com.iwsaitw.easyparking.core.domain.SampleDomain;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SampleService {
 
     public String test() {
-        SampleDomain instance = new SampleDomain();
-
-        return "work";
+        SampleDomain instance = new SampleDomain("work");
+        return instance.getName();
     }
 }
