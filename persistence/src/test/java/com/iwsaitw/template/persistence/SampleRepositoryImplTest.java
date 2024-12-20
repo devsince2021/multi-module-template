ce8ee7cb44b94353a7b7289772fbf0c3ca22183e
devsince2021 <dev.ryu2021@gmail.com>
2024-12-21 01:58:06 +0900

    v1.2.0 (#50)
    * chore: persistence 관련 패키지 범위 최소화
    
    * feat: ContextConfiguration에서 사용할 Config 클래스 생성
    
    * feat: h2 디비를 사용해서 DataJpaTest 동작 확인
    
    * chore: 기존에 1가지로 운영하던 도커 파일을 환경 별로 분리
    
    * refactor: h2에서 도커에 떠있는 데이터베이스로 변경
    
    * add: test 환경변수 내용을 test 도커 컴포즈와 맞춤
    
    * add: 테스트 컨테이너 사용을 위한 의존성 추가
    
    * feat: 단일 테스트 클래스에서 테스트 컨테이너 동작시키는 방법
    
    * add: 또 다른 repository 테스트 추가
    
    * feat: 부모 클래스에서 싱글톤으로 컨테이너 관리하고, @DirtiesContext 사용해서 스프링 컨텍스트 clear
    
    * chore: test 환경의 디비관련 설정 yml은 사용되지 않기 때문에 혼선을 방지하기 위해 주석

---
+++ persistence/src/test/java/com/iwsaitw/template/persistence/SampleRepositoryImplTest.java
@@ -0,0 +1,10 @@
+package com.iwsaitw.template.persistence;
+
+import com.iwsaitw.template.persistence.config.BasePersistenceTest;
+import org.junit.jupiter.api.Test;
+
+public class SampleRepositoryImplTest extends BasePersistenceTest {
+
+    @Test
+    public void contextLoads() {}
+}
