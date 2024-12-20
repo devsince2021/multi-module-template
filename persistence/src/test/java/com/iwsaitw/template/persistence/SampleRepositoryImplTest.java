3734908c327bb62c9474489e6a301b30a4cc8323
devsince2021 <dev.ryu2021@gmail.com>
2024-12-21 01:40:39 +0900

    feat: 부모 클래스에서 싱글톤으로 컨테이너 관리하고, @DirtiesContext 사용해서 스프링 컨텍스트 clear

--- persistence/src/test/java/com/iwsaitw/template/persistence/SampleRepositoryImplTest.java
+++ persistence/src/test/java/com/iwsaitw/template/persistence/SampleRepositoryImplTest.java
@@ -1,37 +1,9 @@
 package com.iwsaitw.template.persistence;
 
-import com.iwsaitw.template.persistence.config.PostgresqlTestConfig;
+import com.iwsaitw.template.persistence.config.BasePersistenceTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@DataJpaTest
-@ContextConfiguration(classes = PostgresqlTestConfig.class)
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Testcontainers
-public class SampleRepositoryImplTest {
-
-    @Container
-    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine")
-            .withDatabaseName("testdb")
-            .withUsername("test")
-            .withPassword("test");
-
-    @DynamicPropertySource
-    static void registerProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
-        registry.add("spring.jpa.show-sql", () -> "true");
-        registry.add("spring.jpa.properties.hibernate.format_sql", () -> "true");
-    }
+public class SampleRepositoryImplTest extends BasePersistenceTest {
 
     @Test
     public void contextLoads() {}
