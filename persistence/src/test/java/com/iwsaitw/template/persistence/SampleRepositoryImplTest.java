ae25b10ec57e32529aa10ef0c0d81e651e243984
devsince2021 <dev.ryu2021@gmail.com>
2024-12-21 01:13:48 +0900

    refactor: h2에서 도커에 떠있는 데이터베이스로 변경

--- persistence/src/test/java/com/iwsaitw/template/persistence/SampleRepositoryImplTest.java
+++ persistence/src/test/java/com/iwsaitw/template/persistence/SampleRepositoryImplTest.java
@@ -2,11 +2,13 @@ package com.iwsaitw.template.persistence;
 
 import com.iwsaitw.template.persistence.config.PostgresqlTestConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ContextConfiguration;
 
 @DataJpaTest
 @ContextConfiguration(classes = PostgresqlTestConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class SampleRepositoryImplTest {
 
     @Test
--- persistence/src/test/java/com/iwsaitw/template/persistence/config/PostgresqlTestConfig.java
+++ persistence/src/test/java/com/iwsaitw/template/persistence/config/PostgresqlTestConfig.java
@@ -1,10 +1,23 @@
 package com.iwsaitw.template.persistence.config;
 
-
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.Primary;
+
+
+import javax.sql.DataSource;
 
 @Import(PostgresqlConfig.class)
-public class PostgresqlTestConfig { }
+public class PostgresqlTestConfig {
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .url("jdbc:postgresql://localhost:55433/single_template_test")
+                .username("user")
+                .password("password")
+                .driverClassName("org.postgresql.Driver")
+                .build();
+    }
+}
