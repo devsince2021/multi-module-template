5568eef2801705b34c2f4abe47fc942418140bed
devsince2021 <dev.ryu2021@gmail.com>
2024-12-21 11:34:10 +0900

    add: 테스트를 위한 find 인터페이스 추가

--- persistence/src/main/java/com/iwsaitw/template/persistence/SampleJpaRepository.java
+++ persistence/src/main/java/com/iwsaitw/template/persistence/SampleJpaRepository.java
@@ -3,6 +3,9 @@ package com.iwsaitw.template.persistence;
 import com.iwsaitw.template.persistence.entity.SampleEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 
 public interface SampleJpaRepository extends CrudRepository<SampleEntity, Long> {
+    Optional<SampleEntity> findByName(String name);
 }
