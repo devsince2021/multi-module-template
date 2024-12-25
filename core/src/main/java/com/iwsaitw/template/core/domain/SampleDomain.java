dd3f790731f34a8bb09b79745146647a21de646c
devsince2021 <dev.ryu2021@gmail.com>
2024-12-25 22:23:41 +0900

    add: base id를 사용한 SmapleDomainId 생성

--- core/src/main/java/com/iwsaitw/template/core/domain/SampleDomain.java
+++ core/src/main/java/com/iwsaitw/template/core/domain/SampleDomain.java
@@ -1,30 +1,39 @@
 package com.iwsaitw.template.core.domain;
 
+import java.util.Objects;
+
 public class SampleDomain {
-    private Long id;
+    private SampleDomainId id;
 
     private String name;
 
     public SampleDomain(Long id, String name) {
-        this.id = id;
+        this.id = new SampleDomainId(id);
         this.name = name;
     }
 
     public SampleDomain(String name) {
-        this.id = 0L;
+        this.id = new SampleDomainId(null);
         this.name = name;
     }
 
     public Long getId() {
-        return id;
+        Long sampleDomainId = id.getId();
+
+        if (Objects.isNull(sampleDomainId)) {
+            throw new IllegalStateException("ID는 null일 수 없습니다");
+        }
+
+        return sampleDomainId;
     }
+
 
     public String getName() {
         return name;
     }
 
     public void setId(Long id) {
-        this.id = id;
+        this.id = new SampleDomainId(id);
     }
 
     public void setName(String name) {
---
+++ core/src/main/java/com/iwsaitw/template/core/domain/SampleDomainId.java
@@ -0,0 +1,10 @@
+package com.iwsaitw.template.core.domain;
+
+import com.iwsaitw.utils.domain.base.BaseId;
+
+public class SampleDomainId extends BaseId {
+
+    protected SampleDomainId(Long id) {
+        super(id);
+    }
+}
