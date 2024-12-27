954232e0031ebec16802d05ea8ab2bfb70c9dd11
devsince2021 <dev.ryu2021@gmail.com>
2024-12-27 22:38:21 +0900

    add: 변경 사항 반영

--- persistence/src/test/java/com/iwsaitw/template/persistence/OtherSampleRepositoryImplTest.java
+++ persistence/src/test/java/com/iwsaitw/template/persistence/OtherSampleRepositoryImplTest.java
@@ -1,9 +1,11 @@
 package com.iwsaitw.template.persistence;
 
-import com.iwsaitw.template.persistence.config.BasePersistenceTest;
+import com.iwsaitw.template.persistence.config.DefaultPersistenceConfig;
 import com.iwsaitw.template.persistence.entity.SampleEntity;
+import com.iwsaitw.utils.persistence.testcontainer.BasePersistenceTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
@@ -12,6 +14,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ContextConfiguration(classes = DefaultPersistenceConfig.class)
 public class OtherSampleRepositoryImplTest extends BasePersistenceTest {
 
     @DynamicPropertySource
