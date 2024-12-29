c7346685a7429a11ada2b327f41b1afb111f84dd
devsince2021 <dev.ryu2021@gmail.com>
2024-12-29 22:31:31 +0900

    chore: 잘못된 패키지 수정

--- base/persistence/src/test/java/com/iwsaitw/base/persistence/base/config/BaseEntityTestConfig.java
+++ base/persistence/src/test/java/com/iwsaitw/base/persistence/base/config/BaseEntityTestConfig.java
@@ -1,6 +1,5 @@
 package com.iwsaitw.base.persistence.base.config;
 
-import com.iwsaitw.base.persistence.config.BaseEntityConfig;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
